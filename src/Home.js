ae8a811b248d8c213edafe128b9b89ac3a400b09
Patrick Haimbaugh <phaimbaugh2000@gmail.com>
2024-06-08 18:27:29 -0700

    remove local delete

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,6 @@ import BlogList from "./BlockList";
 const Home = () => {
     const [blogs, setBlogs] = useState([])
 
-    const handleDelete = (blogId) => {
-        const newBlogs = blogs.filter((blog) => blog.id !== blogId)
-        setBlogs(newBlogs)
-    }
-
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
@@ -21,7 +16,7 @@ const Home = () => {
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
+            <BlogList blogs={blogs} title="All blogs" />
         </div>
     );
 }
